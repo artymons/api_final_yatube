d1b684213b43a31b6673c253fa767cbac8ddb820
Артемий Винн <hoztov25@ya.ru>
2022-09-09 21:05:15 +0300

    second21

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -25,8 +25,6 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
